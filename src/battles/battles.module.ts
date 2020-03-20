0856009bdfc7832a0b88eb0f89769616d705d7c3
cgloveut <pitsanubomb@gmail.com>
2020-03-21 00:06:12 +0700

    :fire: Add somelist for support battle

--- src/battles/battles.module.ts
+++ src/battles/battles.module.ts
@@ -3,9 +3,11 @@ import { BattlesResolver } from "./battles.resolver";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Battle } from "./entity/battles.entity";
 import { BattlesService } from "./battles.service";
+import { BattlesController } from "./battles.controller";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Battle])],
-  providers: [BattlesResolver,BattlesService]
+  providers: [BattlesResolver,BattlesService],
+  controllers:[BattlesController]
 })
 export class BattlesModule {}
--- src/battles/input/battles.input.ts
+++ src/battles/input/battles.input.ts
@@ -7,15 +7,21 @@ export class BattleInput {
   @Field()
   readonly Battlename: string;
   @Field()
-  readonly Battlecover: string;
-  @Field()
   readonly Battlestatus: string;
   @Field(() => Int)
   readonly vj1_ccuteid: number;
   @Field(() => Int)
   readonly vj2_ccuteid: number;
   @Field()
+  readonly vj1_cover: string;
+  @Field()
+  readonly vj2_cover: string;
+  @Field()
   readonly vj1_score: number;
   @Field()
   readonly vj2_score: number;
+  @Field()
+  readonly battletime: Date;
+  @Field()
+  readonly channelid:string;
 }
