82b31b7ecbbc031eb2d23020349d2c7be8900e0a
cgloveut <pitsanubomb@gmail.com>
2020-04-25 03:26:02 +0700

    🔥 Remove notuse  🔥

--- src/battles/battles.controller.ts
+++ src/battles/battles.controller.ts
@@ -1,27 +1,9 @@
-import { Controller, Get, Param, Header, Post, Res, Put } from "@nestjs/common";
-import { Response } from "express";
+import { Controller, Get, Param, Put } from "@nestjs/common";
 import { BattlesService } from "./battles.service";
-import { EventEmitter } from "events";
 
-const eventEmitter = new EventEmitter();
 @Controller("battles")
 export class BattlesController {
   constructor(private readonly battleService: BattlesService) {}
-  @Header("Content-Type", "text/event-stream")
-  @Header("Connection", "keep-alive")
-  @Header("Cache-Control", "no-cache")
-  @Get(":id")
-  findId(@Res() res: Response, @Param("id") id: string) {
-    eventEmitter.on(id, data => {
-      res.write('data: {"msg": ' + data + "}\n\n");
-    });
-    res.write("");
-  }
-
-  @Post(":id")
-  post(@Param("id") id: string) {
-    eventEmitter.emit(id, id);
-  }
 
   @Get()
   async findall(): Promise<any[]> {
